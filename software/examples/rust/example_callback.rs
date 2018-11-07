68ce955f503ebb487e70e7e4616b5ef87ab4d4e7
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:23:27 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,40 +1,41 @@
 use std::{error::Error, io, thread, time::Duration};
-use tinkerforge::{ipconnection::IpConnection, laser_range_finder_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, laser_range_finder_bricklet::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Laser Range Finder Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Laser Range Finder Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let laser_range_finder_bricklet = LaserRangeFinderBricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let lrf = LaserRangeFinderBricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
     // Turn laser on and wait 250ms for very first measurement to be ready
-    laser_range_finder_bricklet.enable_laser();
+    lrf.enable_laser();
     thread::sleep(Duration::from_millis(250));
 
-    //Create listener for distance events.
-    let distance_listener = laser_range_finder_bricklet.get_distance_receiver();
-    // Spawn thread to handle received events. This thread ends when the laser_range_finder_bricklet
+    // Create receiver for distance events.
+    let distance_receiver = lrf.get_distance_receiver();
+
+    // Spawn thread to handle received events. This thread ends when the `lrf` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in distance_listener {
-            println!("Distance: {}{}", event, " cm");
+        for distance in distance_receiver {
+            println!("Distance: {} cm", distance);
         }
     });
 
-    // Set period for distance listener to 0.2s (200ms)
+    // Set period for distance receiver to 0.2s (200ms).
     // Note: The distance callback is only called every 0.2 seconds
     //       if the distance has changed since the last call!
-    laser_range_finder_bricklet.set_distance_callback_period(200);
+    lrf.set_distance_callback_period(200);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
     io::stdin().read_line(&mut _input)?;
-    laser_range_finder_bricklet.disable_laser(); // Turn laser off
+    lrf.disable_laser(); // Turn laser off
     ipcon.disconnect();
     Ok(())
 }
--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -1,41 +1,42 @@
 use std::{error::Error, io, thread, time::Duration};
-use tinkerforge::{ipconnection::IpConnection, laser_range_finder_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, laser_range_finder_bricklet::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Laser Range Finder Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Laser Range Finder Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let laser_range_finder_bricklet = LaserRangeFinderBricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let lrf = LaserRangeFinderBricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
     // Turn laser on and wait 250ms for very first measurement to be ready
-    laser_range_finder_bricklet.enable_laser();
+    lrf.enable_laser();
     thread::sleep(Duration::from_millis(250));
 
-    // Get threshold listeners with a debounce time of 10 seconds (10000ms)
-    laser_range_finder_bricklet.set_debounce_period(10000);
+    // Get threshold receivers with a debounce time of 10 seconds (10000ms).
+    lrf.set_debounce_period(10000);
 
-    //Create listener for distance reached events.
-    let distance_reached_listener = laser_range_finder_bricklet.get_distance_reached_receiver();
-    // Spawn thread to handle received events. This thread ends when the laser_range_finder_bricklet
+    // Create receiver for distance reached events.
+    let distance_reached_receiver = lrf.get_distance_reached_receiver();
+
+    // Spawn thread to handle received events. This thread ends when the `lrf` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in distance_reached_listener {
-            println!("Distance: {}{}", event, " cm");
+        for distance_reached in distance_reached_receiver {
+            println!("Distance: {} cm", distance_reached);
         }
     });
 
-    // Configure threshold for distance "greater than 20 cm"
-    laser_range_finder_bricklet.set_distance_callback_threshold('>', 20, 0);
+    // Configure threshold for distance "greater than 20 cm".
+    lrf.set_distance_callback_threshold('>', 20, 0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
     io::stdin().read_line(&mut _input)?;
-    laser_range_finder_bricklet.disable_laser(); // Turn laser off
+    lrf.disable_laser(); // Turn laser off
     ipcon.disconnect();
     Ok(())
 }
